0812b7e323382802843963547a5cb0b530d65406
Omar Gamel <omar.gamel@baianat.com>
2022-12-05 17:36:37 +0200

    first commit

---
+++ util/file.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const deleteFile = (filePath) => {
+  if (filePath) {
+    fs.unlink(path.join(__dirname, '..', filePath), (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  }
+};
+
+exports.deleteFile = deleteFile;
